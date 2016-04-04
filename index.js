4d053efe5525c88bb7e8be82a8fc521de26fda6d
Emmanuel Desir <edesir@enablon.net>
2016-04-04 16:06:23 +0200

    film names are clean

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Hello World");
+console.log("++++++++++++++++Sorting Movies+++++++++++++");
+var recursive = require('recursive-readdir');
+var path = require("path");
+
+var repertoryRoot = "C:\\Users\\edesir\\Documents\\Boulot\\Films";
+var authorizedExtensions = ['.flv', '.mpg'];
+var fileReleasePatterns = ['DVDRip', 'DVDSCR', 'XviD-MAXSPEED'];
+
+
+function isNotFilm(file, stats) {
+    var fileExtension = path.extname(file);
+
+    return (!stats.isDirectory()) && (authorizedExtensions.indexOf(fileExtension) === -1);
+}
+
+function cleanFilmName(filePath) {
+    var filmName = filePath
+        // file name
+        .replace(/^.*[\\\/]/, '')
+        // no extension
+        .replace(/\.[^/.]+$/, '')
+        // no release date
+        .replace(/\(([^)]+)\)/, '');
+    // no file relase terms
+    fileReleasePatterns.forEach(function(fileReleasePattern) {
+        filmName = filmName.replace(fileReleasePattern, '');
+    });
+    // trim
+    filmName = filmName.trim();
+    
+    return {
+        filmName,
+        filePath
+    }
+}
+
+
+recursive(repertoryRoot, [isNotFilm], function (err, files) {
+  // Files is an array of filename
+  files.forEach(function(filePath) {
+    var probableFilmNameAndPath = cleanFilmName(filePath);
+    console.log(probableFilmNameAndPath);
+
+  });
+});
+
+
+
+
+
